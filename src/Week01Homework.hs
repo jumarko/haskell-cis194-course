1f2f81bfa8bdf9248b42b26f4b16d04619f2ce62
Juraj Martinka <jumarko@gmail.com>
2017-07-03 12:20:23 +0200

    Week 1: Homework - first part (credit card number validation).

---
+++ src/Week01Homework.hs
@@ -0,0 +1,40 @@
+-- Week 1 Homework
+-- http://www.seas.upenn.edu/~cis194/spring13/hw/01-intro.pdf
+
+-- Validating credit card numbers:
+-- Double the value of every second digit beginning from the right: e.g. [1,3,8,6] -> [2,3,16,6]
+-- Add the digits of doubles values and the undoubled digits from the original number:
+--   [2,3,16,6] -> 2 + 3 + 1 + 6 + 6 = 18
+-- Calculate the remainder when the sum is divided by 10
+
+-- Exercise 1: We need to first find the digits of a number
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev n
+  | n < 0 = []
+  | n < 10 = [n]
+  | otherwise = (mod n 10) : toDigitsRev (div n 10)
+
+toDigits :: Integer -> [Integer]
+toDigits n = reverse(toDigitsRev n)
+
+
+-- Exercise 2: Double every other digit
+doubleEveryOtherFromLeft :: [Integer] -> [Integer]
+doubleEveryOtherFromLeft [] = []
+doubleEveryOtherFromLeft [x] = [x]
+doubleEveryOtherFromLeft (x:y:zs) = x : (2*y) : (doubleEveryOtherFromLeft zs)
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther = reverse . doubleEveryOtherFromLeft . reverse
+
+
+-- Exercise 3: define sum of digits
+sumDigits :: [Integer] -> Integer
+sumDigits [] = 0
+sumDigits (x:ys) = (mod x 10) + (div x 10) + (sumDigits ys)
+
+
+-- Exercise 4: validate function indicates whether an Integer
+-- could be a valid credit card number
+validate :: Integer -> Bool
+validate x = (mod (sumDigits (doubleEveryOther (toDigits x))) 10) == 0
